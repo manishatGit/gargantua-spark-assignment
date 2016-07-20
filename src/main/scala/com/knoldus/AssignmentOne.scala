adf9859d3f1ee9d32231946a396aeb551f293c35
manishAtGit <manish@knoldus.com>
2016-07-20 18:51:19 +0530

    corrected question 5

--- src/main/scala/com/knoldus/AssignmentOne.scala
+++ src/main/scala/com/knoldus/AssignmentOne.scala
@@ -14,7 +14,7 @@ object AssignmentOne extends App {
   println("Total records in the file::: "+totalRecords)
   val englishPageCounts = rdd.filter( line => line.split(" ").apply(0).contains("en")) //Q4
   println("Total records with english pages::: "+englishPageCounts.count()) //Q5
-  val pageWiseRequestCount = rdd.map(line => (line.split(" ").apply(0),1)).reduceByKey((a,b)=> a+b)
+  val pageWiseRequestCount = rdd.map(line => (line.split(" ").apply(0),line.split(" ").apply(2).toInt)).reduceByKey((a,b)=> a+b)
   val pagesWithRequestMoreThanTwoHundredThousands = pageWiseRequestCount.filter(_._2>200000).count()//Q6
   println("Total pages which were requested more than 200000::"+pagesWithRequestMoreThanTwoHundredThousands)
   sc.stop()
